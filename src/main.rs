1bcf3550a56cd551cda2681a8b99c787396b1777
mzh-tbg <hussainmuhammadzain@gmail.com>
2024-06-14 22:40:32 +0100

    6

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,44 @@
 mod read_input;
+
 //use capitalize::Capitalize;
 use read_input::read_input;
+//use std::collections::HashMap;
 
 fn main() {
 
-    let mut fruits: Vec<f64> = Vec::new();
-    
-    let user: u32 = read_input("How many fruits do you have: ")
-        .parse()
-        .expect("Please enter a sensible number");
-
-    let mut count = 0;
+    let mut lives = 3;
+    let mut level = 0;
+    let mut points = 0;
 
     loop {
-        if count == user {break;}
-
-        let fruit_w: i64 = read_input("What is the whight of your fruit to the nearest grams: ")
-            .parse()
-            .expect("Please enter a sensible number!");
-
-        if fruit_w < 0 {println!("Enter a positive whole number!");continue;}
-        else {
-            count += 1;
-            fruits.push(fruit_w as f64);
+        if points == 20 || lives == 0 {break;}
+        let question: u32 = read_input("Did you:\n1- Lose a life\n2-Gain a point: ")
+            .parse().expect("Please enter a sensible positive ineger!");
+        
+        match question {
+            1 => {
+                lives -= 1;
+                println!("You now have a total of {} lives", lives);
+            },
+            2 => {
+                points += 1;
+                if points % 5 == 0 {
+                    level +=1;
+                    println!("You have gotten up a level!")
+                }
+                println!("Total points {}", points);
+            }
+            _ => {
+                println!("Enter a number fom 1-2!");
+                continue; 
+            }
         }
+
+
     }
-
-    let sum: f64 = fruits.iter().sum();
-
-    println!("Average weight for the fruits is {}", sum/(user as f64));
-    
     
 
-
+    println!("You have gotten to level ({}) with {} lives and {} points!", level, lives, points);
 
 }
 
